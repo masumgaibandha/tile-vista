c69f14cebb1bf5906f179588c12be454c298fc01
masumgaibandha <masumgaibandha@gmail.com>
2026-05-02 20:41:32 +0600

    updated profile

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+"use client";
+import { UpdateUserModal } from "@/component/UpdateProfileModal";
+import { authClient } from "@/lib/auth-client";
+import { Avatar, Card } from "@heroui/react";
 
 const ProfilePage = () => {
-    return (
-        <div>
-            Profile Page
-        </div>
-    );
+  const userData = authClient.useSession();
+  const user = userData.data?.user;
+
+  return (
+    <div>
+      <Card className=" max-w-96 mx-auto flex flex-col items-center my-6 border border-gray-200">
+        <Avatar className="w-20 h-20">
+          <Avatar.Image
+            alt="User name"
+            src={user?.image}
+            referrerPolicy="no-referrer"
+          />
+          <Avatar.Fallback>{user?.name[0]}</Avatar.Fallback>
+        </Avatar>
+        <h2 className="text-xl font-bold">{user?.name}</h2>
+        <p className="text-muted">{user?.email}</p>
+        <UpdateUserModal/>
+      </Card>
+    </div>
+  );
 };
 
 export default ProfilePage;
