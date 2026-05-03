f7302569df3dc483b9d9b6d433563d5fea9106aa
masumgaibandha <masumgaibandha@gmail.com>
2026-05-03 11:19:48 +0600

    fix vercel preview auth host2s

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,11 +1,20 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db("tiles-vista");
+
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
     client,
   }),
 
-  allowedHosts: ["localhost:*", "tile-vista.vercel.app", "*.vercel.app"],
-
-  trustedOrigins: ["http://localhost:3000", "https://tile-vista.vercel.app"],
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://tile-vista.vercel.app",
+    "https://*.vercel.app",
+  ],
 
   emailAndPassword: {
     enabled: true,
